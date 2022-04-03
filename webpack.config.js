0813164750e1839e120241198aafe22d00bc72b8
Ming-der Wang <mingderwang@gmail.com>
2022-04-03 21:59:11 +0800

    add shoelace

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
-const path = require('path');
+const { WebpackPluginServe: Serve } = require("webpack-plugin-serve");
+const { join } = require("path");
+const outputPath = join(process.cwd(), "/dist");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const options = { port: 8000 };
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  module: {
+    // Bundle styles into main.css
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
-  mode: 'development',
+  mode: "development",
+  // an example entry definition
+  entry: [
+    "webpack-plugin-serve/client", // ← important: this is required, where the magic happens in the browser
+    "./src/index.js",
+  ],
+  output: {
+    filename: "main.js",
+    path: outputPath,
+  },
+  plugins: [new MiniCssExtractPlugin(), new Serve(options)],
+  watch: true, // ← important: webpack and the server will continue to run in watch mode
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
 };
